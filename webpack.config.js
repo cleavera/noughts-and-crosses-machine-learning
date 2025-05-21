b748be4aa34889f31fed395059ff8955c7060499
Anthony Cleaver <Anthony.Cleaver@mhrglobal.com>
2025-05-21 17:07:51 +0100

    remove sass dependency

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,6 @@ module.exports = {
                 test: /\.scss$/,
                 use: [
                     'raw-loader',
-                    'sass-loader'
                 ]
             }
         ]
